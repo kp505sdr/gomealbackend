53fe1542d4626e8cb2ba82f6ac9621600072db69
Karan <Karandp505@gmail.com>
2023-04-14 12:45:38 +0530

    add change

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 
-const app =require("./app")
-const connectDatabase=require("./src/configs/database")
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors =require('cors')
+const app = express()
+// user:
+// karandp0011
+// pass:
+// Kp505123
 
-//config
 
+app.use(bodyParser.json({limit: '25mb'}))
+app.use(bodyParser.urlencoded({limit: '25mb', extended: false}))
+app.use(cors());
+app.set("view engine", "ejs");
 
-//database connection
-connectDatabase()
-// mongoose.connect(DB).then(()=>{
-//   console.log(`connection successfull`);
-// }).catch((err)=>{
-//   console.log("no connection")
-// })
-   
-app.listen(3000,()=>{
-  console.log(`server is working on http://localhost:${3000}`)
-})
+//Main Route Import
+const all_routs=require("./src/routes/studentRoot")
+
+app.use("/api/v1",all_routs)
+
+module.exports=app
