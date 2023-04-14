5649e3d86e747764a8241801e586550471952455
Karan <Karandp505@gmail.com>
2023-04-14 13:05:10 +0530

    remove server file

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
-const server =require("./server")
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors =require('cors')
 const connectDatabase=require("./src/configs/database")
 
 //config
 
-
+const app = express()
 //database connection
 connectDatabase()
 // mongoose.connect(DB).then(()=>{
@@ -13,6 +15,28 @@ connectDatabase()
 //   console.log("no connection")
 // })
    
-server.listen(3000,()=>{
+
+
+// user:
+// karandp0011
+// pass:
+// Kp505123
+
+
+app.use(bodyParser.json({limit: '25mb'}))
+app.use(bodyParser.urlencoded({limit: '25mb', extended: false}))
+app.use(cors());
+app.set("view engine", "ejs");
+
+//Main Route Import
+const all_routs=require("./src/routes/studentRoot")
+
+app.use("/api/v1",all_routs)
+
+
+
+
+
+app.listen(3000,()=>{
   console.log(`server is working on http://localhost:${3000}`)
 })
